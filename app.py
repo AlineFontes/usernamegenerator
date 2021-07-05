9da6f2edf781952b215228458522759d065e5e1a
alinefontes <alinecamili@gmail.com>
2021-07-04 22:19:33 -0700

    implemented the user name generator

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-def generate_user_name(seed: str) -> str:
-    pass
+from random import shuffle
+
+
+def generate_user_name(name: str) -> str:
+    #splitting the string into a list of chars
+    unique_letters = list(set(name))
+
+    # shuffle letters
+    shuffle(unique_letters)
+
+    # join letters into a string
+    nick_name = "".join(unique_letters)
+
+    #limit to seven letters
+    nick_name = nick_name[0:8]
+
+    return nick_name
